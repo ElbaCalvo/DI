af997c5b31f24b8fbc137602fcf70237a4748d2e
Elba Calvo <ecalvog@fpcoruna.afundacion.org>
2023-09-27 10:17:20 +0200

    Ejercicio operaciones.

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,11 @@
+def suma(i, j):
+    return i+j
+def resta(i, j):
+    return i-j
+def multiplicacion(i, j):
+    return i*j
+def division(i, j):
+    if j == 0:
+        return "[!] No se puede dividir entre cero. \n"
+    else:
+        return i/j
