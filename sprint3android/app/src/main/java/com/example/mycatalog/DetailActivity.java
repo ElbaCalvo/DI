4e8ad0700d0b20508d60a43a6f895b691b0ffe0e
Elba Calvo <ecalvog@fpcoruna.afundacion.org>
2023-10-31 10:06:23 +0100

    Ejercicio 3 pantalla detail.

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,10 +1,19 @@
 package com.example.mycatalog;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageView imagen;
+    private TextView descripcion;
+    private Button meGustaButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // Llama al método onCreate.
